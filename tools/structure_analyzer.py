f83164ed70fb236a890e9621503db7c062916d9e
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2026-03-13 16:04:48 +0530

    structure_analyzer 1st version

--- tools/structure_analyzer.py
+++ tools/structure_analyzer.py
@@ -1,30 +1,5 @@
 import pandas as pd
 
-
-def analyze_structure(df):
-
-    structure = {
-        "numerical_columns": [],
-        "categorical_columns": [],
-        "datetime_columns": []
-    }
-
-    for column in df.columns:
-
-        if pd.api.types.is_numeric_dtype(df[column]):
-            structure["numerical_columns"].append(column)
-
-        elif pd.api.types.is_datetime64_any_dtype(df[column]):
-            structure["datetime_columns"].append(column)
-
-        else:
-            structure["categorical_columns"].append(column)
-
-    return structure
-
-import pandas as pd
-
-
 def df_to_column_samples(df, sample_size=10):
 
     column_samples = {}
